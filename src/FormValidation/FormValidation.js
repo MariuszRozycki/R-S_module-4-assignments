a8b98b7b0f329d0a21bb68082887876315c1d0ea
MariuszRozycki <mariansc83@icloud.com>
2022-06-26 17:00:11 +0200

    feat: form validation ready. No input type:ratio validation

--- src/FormValidation/FormValidation.js
+++ src/FormValidation/FormValidation.js
@@ -4,35 +4,82 @@ import '../App.css';
 function FormValidation() {
 
   const [nameInput, setNameInput] = useState('');
-  console.log('nameInput', nameInput);
+  const [emailInput, setEmailInput] = useState('');
+  const [bioInput, setBioInput] = useState('');
+
+  const nameInputErr = useRef();
   const nameError = useRef();
 
-  const inputNameHandle = (e) => {
+  const emailInputErr = useRef();
+  const emailError = useRef();
+
+  const bioAreaErr = useRef();
+  const bioError = useRef();
+
+
+  const nameInputHandle = (e) => {
     setNameInput(e.target.value);
+  }
+
+  const emailInputHandle = (e) => {
+    setEmailInput(e.target.value);
+  }
+
+  const checkEmail = (email) => {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+  }
+
+  const bioInputHandle = (e) => {
+    setBioInput(e.target.value);
   }
 
   const handleForm = (e) => {
     e.preventDefault();
 
-    if (nameInput.length < 5) {
-      nameError.current.style.display = 'block'
+    if (nameInput.trim().length < 5) {
+      nameInputErr.current.style.border = '2px solid red';
+      nameError.current.style.display = 'block';
+    } else {
+      nameInputErr.current.style.border = '1px solid black';
+      nameError.current.style.display = 'none';
+    }
+
+    if (!checkEmail(emailInput)) {
+      emailInputErr.current.style.border = '2px solid red';
+      emailError.current.style.display = 'block';
+    }
+    else {
+      emailInputErr.current.style.border = '1px solid black';
+      emailError.current.style.display = 'none';
+    }
+
+    if (bioInput.trim().length < 11) {
+      bioAreaErr.current.style.border = '2px solid red';
+      bioError.current.style.display = 'block';
+    }
+    else {
+      bioAreaErr.current.style.border = '1px solid black';
+      bioError.current.style.display = 'none';
     }
   }
+
 
   return (
     <form className="form">
       <h1>FormValidation</h1>
       <label htmlFor="name">Name (Min. 5 characters required)
-        <input id="name" name="name" onChange={inputNameHandle} />
-        <p ref={nameError} className="error-text">Field is required - min. 5 characters</p>
+        <input ref={nameInputErr} id="name" name="name" onChange={nameInputHandle} />
+        <p ref={nameError} className="error">Field is required - min. 5 characters</p>
       </label>
       <label htmlFor="e-mail">E-mail
-        <input id="e-mail" name="e-mail" />
-        <p className="error-text">Field is required</p>
+        <input ref={emailInputErr} id="e-mail" name="e-mail" onChange={emailInputHandle} />
+        <p ref={emailError} className="error">Field is required</p>
       </label>
       <label htmlFor="bio">Write down something about bio:
-        <textarea name="bio" id="bio"></textarea>
-        <p className="error-text">Field is required</p>
+        <textarea ref={bioAreaErr} name="bio" id="bio" onChange={bioInputHandle} ></textarea>
+        <p ref={bioError} className="error">Field is required</p>
       </label>
       <fieldset>
         <legend>Choose your sex:</legend>
@@ -43,7 +90,6 @@ function FormValidation() {
           <input type="radio" name="sex" id="woman" />
         </label>
       </fieldset>
-      <p className="error-text">Field is required</p>
       <button type="submit" onClick={handleForm}>Send</button>
     </form>
   )
