f1ec99f53525604cf9c6c7be586b91bac8d43442
MariuszRozycki <mariansc83@icloud.com>
2022-06-27 23:54:17 +0200

    feat: uncorrect validation

--- src/FormValidation/FormValReactForm.js
+++ src/FormValidation/FormValReactForm.js
@@ -10,15 +10,41 @@ function ReactFormValidation() {
     formState: { errors }
   } = useForm();
 
-  const refErrNoAt = useRef();
+
   const [emailInput, setEmailInput] = useState();
   const [flag, setFlag] = useState(false);
+  const [radioMan, setRadioMan] = useState(false);
+  const [radioWoman, setRadioWoman] = useState(false);
+
+  const refErrNoAt = useRef();
+  const radioErr = useRef();
+  const radioErrMessage = useRef();
 
   const emailHandler = (event) => {
     setEmailInput(event.target.value)
   }
 
-  const handleData = (data) => {
+  const radioManHandler = (e) => {
+    setRadioMan(true);
+  }
+
+  const radioWomanHandler = (e) => {
+    setRadioWoman(true);
+  }
+
+  // Nie moge uzyc w useForm, useRef w taki sam sposob, jak w kodzie bez useForm. Kiedy wstawie {...register("name")}, to nie moge uzywac ref-ow na tych elementach. Nie moge tez uzyc wlasciwosci length na elemencie z {...register("")} Z powodu useForm nie moge zrobic takiej validacji jaka chce, tak jak w komponencie FormValidation.
+  const handleRadio = () => {
+    if (radioMan || radioWoman) {
+      console.log('radioMan || radioWoman', radioMan || radioWoman);
+      radioErr.current.style.border = '1px solid black';
+      radioErrMessage.current.style.display = 'none';
+    } else {
+      radioErr.current.style.border = '2px solid red';
+      radioErrMessage.current.style.display = 'block';
+    }
+  }
+
+  const onSubmit = (data, handleRadio) => {
     if (!emailInput.includes('@')) {
       console.error(`Mail address doesn't include @ - symbol`);
       refErrNoAt.current.style.display = 'block';
@@ -26,7 +52,7 @@ function ReactFormValidation() {
       refErrNoAt.current.style.display = 'none';
     }
 
-    if (data && emailInput.includes('@')) {
+    if (data && emailInput.includes('@') && handleRadio) {
       setFlag(true);
     }
   }
@@ -41,7 +67,7 @@ function ReactFormValidation() {
   }
 
   return (
-    <form onSubmit={handleSubmit(handleData)} className="form">
+    <form onSubmit={handleSubmit(onSubmit, handleRadio)} className="form">
       <h1>ReactFormVal</h1>
       <label htmlFor="userName">Name (Min. 2 character required)
         <input id="userName" placeholder="Type your name" {...register("name", {
@@ -70,15 +96,16 @@ function ReactFormValidation() {
         })} id="userBio" ></textarea>
         <p className="error-react_form">{errors.bio?.message}</p>
       </label>
-      <fieldset>
+      <fieldset ref={radioErr}>
         <legend>Choose your sex:</legend>
         <label htmlFor="man_react-val">Man:
-          <input type="radio" name="choose-sex" id="man_react-val" />
+          <input type="radio" name="choose-sex" id="man_react-val" onChange={radioManHandler} />
         </label>
         <label htmlFor="woman_react-val">Woman:
-          <input type="radio" name="choose-sex" id="woman_react-val" />
+          <input type="radio" name="choose-sex" id="woman_react-val" onChange={radioWomanHandler} />
         </label>
       </fieldset>
+      <p ref={radioErrMessage} className="error">Field is required!</p>
       <input type="submit" value="Submit React Form" />
     </form>
   )
